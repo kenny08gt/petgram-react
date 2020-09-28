c86a86e67ad5570a2084935a30d12745639a3ca9
Alan Hurtarte <alan@mypostcard.com>
2020-09-28 01:55:14 -0600

    test

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState(mockCategories)
+
+  // useEffect(function() {
+  //   fetch('')
+  // })
   return (
     <List>
       {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
