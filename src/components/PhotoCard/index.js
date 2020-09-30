2d02c8315a0925ec9c127bfbf87218460707e607
Alan Hurtarte <alan@mypostcard.com>
2020-09-30 00:25:33 -0600

    added polyfill for intersectionObserver

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,15 +9,19 @@ export const PhotoCard = ({ id, likes = 0, src = DEFUALT_IMG }) => {
   const [show, setShow] = useState(false)
 
   useEffect(function () {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
-    })
-
-    observer.observe(ref.current)
+    Promise.resolve(typeof window.IntersectionObserver !== 'undefined'
+      ? window.IntersectionObserver
+      : import('intersection-observer'))
+      .then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(ref.current)
+      })
   }, [ref])
 
   return (
