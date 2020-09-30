89baf93f8b67460bcf7acb5b8283da4140cec826
Alan Hurtarte <alan@mypostcard.com>
2020-09-30 00:01:23 -0600

    added list category loader

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,55 @@
 import React, { useEffect, useState } from 'react'
+import MyLoader from './loader'
 
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories as mockCategories } from '../../../api/db.json'
+// import { categories as mockCategories } from '../../../api/db.json'
+
+function useCategoreisData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('http://petgram-server-alan.alan-hurtarte.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setLoading(false)
+        setCategories(response)
+      })
+  }, [])
+
+  return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState(mockCategories)
+  const { categories, loading } = useCategoreisData()
 
-  // useEffect(function() {
-  //   fetch('')
-  // })
-  return (
-    <List>
-      {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        loading ? <MyLoader />
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -5,7 +5,7 @@ import { Ul } from './styles'
 export const ListOfPhotoCards = () => {
     return (
         <Ul>
-            {[1 , 2, 3].map( id => <PhotoCard key={id} />)}
+            {[1 , 2, 3, 4, 5, 6].map( id => <PhotoCard key={id} />)}
         </Ul>
     )
 }
